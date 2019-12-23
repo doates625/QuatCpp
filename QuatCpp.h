863251939271f0745e54058aea646304f4c558f1
Dan <doates625@gmail.com>
2019-12-23 12:02:04 -0500

    First commit

---
+++ QuatCpp.h
@@ -0,0 +1,12 @@
+/**
+ * @file QuatCpp.h
+ * @brief Include file for QuatCpp quaternion extension of LinearCpp
+ * @author Dan Oates (WPI Class of 2020)
+ */
+#pragma once
+
+// LinearCpp Include
+#include <LinearCpp.h>
+
+// Quaternion Includess
+// TODO
